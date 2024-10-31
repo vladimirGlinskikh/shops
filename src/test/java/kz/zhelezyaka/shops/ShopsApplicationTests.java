b2d03141efa819c95839fbc7b880bf9739b780f7
Vladimir Glinskikh <vladimir.v.glinskikh@gmail.com>
2024-10-31 17:48:10 +0300

    Start project

---
+++ src/test/java/kz/zhelezyaka/shops/ShopsApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.zhelezyaka.shops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
